45825e03e20afd3c5b8473c819b90e6a98cd219e
koteyka5000 <romacat5000@gmail.com>
2021-06-08 15:52:05 +0300

    Добавлена логика игры. v1.0

--- Hagman.py
+++ Hagman.py
@@ -23,13 +23,34 @@ def get_word(w):
     return w[0]
 
 
+def replace(inp, word, template):
+    for i in range(len(word)):
+        if inp == word[i]:
+            template[i] = inp
+    return template
+
 def game():
     progress = True
-    word = ['orange']
+    word = ['oreaenegeeeg']
     lifes = 3
 
     word_in_play = get_word(word)
     template = start_template(word_in_play)
     welcome_speech(list_to_string_convert(template))
-
+    while progress:
+        print('======================')
+        print(f'У вас осталось попыток: {lifes}')
+        print(f'Сейчас слово: {list_to_string_convert(template)}')
+        inp = input('Введите предпологаемую букву: ')
+        if inp in word_in_play:
+            template = replace(inp, word_in_play, template)
+        else:
+            print('Такой буквы в слове нет :(')
+            lifes -= 1
+        if lifes == 0 or word_in_play == template:
+            progress = False
+    if lifes == 0:
+        print(f'Вы хорошо держались :)\nЗагаданым словом было {word_in_play}')
+    elif word_in_play == template:
+        print(f'Вы победили!! :)\nУ вас осталось попыток: {lifes}')
 game()
