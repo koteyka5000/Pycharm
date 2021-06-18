9a858e92dfa08b04051bbbb08859b422ee7ab7d1
koteyka5000 <romacat5000@gmail.com>
2021-06-18 15:42:33 +0300

    :)

---
+++ 8Lesson1.py
@@ -0,0 +1,10 @@
+q = input()
+listq = list(q)
+l = 0
+u = 0
+for lol in listq:
+    if lol.islower():
+        l += 1
+    elif lol.isupper():
+        u += 1
+print(f'Строчные: {l}, Заглавные: {u}')
