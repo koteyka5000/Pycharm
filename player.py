a8bdf050a1527ea1cfbff503de86bb0bdeaabb09
koteyka5000 <romacat5000@gmail.com>
2021-06-18 15:30:39 +0300

    Есть баг при выходе за границы поля, но не очень критичный v1.0

--- player.py
+++ player.py
@@ -1,15 +1,36 @@
+from random import choice
+
+
 def get_area(s):
-    area = [['X', '.', '.', '.'],
-            ['.', 'o', '.', '.'],
-            ['.', '.', '.', 'o'],
-            ['.', '.', '.', '.']]
+    area = []
+    s -= 1
+    f = True
+    tmp = ['X']
+    allq = ['_', '_', '_', 'o']
+    for i in range(s):
+        for j in range(s):
+            tmp.append(choice(allq))
+        area.append(tmp)
+        tmp = []
+        if f:
+            s += 1
+        f = False
+
+    # area = [['X', '.', '.', '.'],
+    #         ['.', 'o', '.', '.'],
+    #         ['.', '.', '.', 'o'],
+    #         ['.', '.', '.', '.']]
+    print(area)
     return [area, s]
 
 
 def print_map(area):
     print('==========')
-    print(*area[0], '\n', *area[1], '\n', *area[2], '\n', *area[3], sep='')
+    for i in range(len(area)):
+        # print(*area[0], '\n', *area[1], '\n', *area[2], '\n', *area[3], sep='')
+        print(*area[i], sep=' ')
     print('==========')
+
 
 def game(areaq):
     area = areaq[0]
@@ -19,59 +40,80 @@ def game(areaq):
     nowy = 0
     while True:
         where = input('wasd: ')
+        # def check_zero():
+        #     global nowx, nowy
+        #     if nowx < 0:
+        #         nowx = area_len
+        #     elif nowx > area_len:
+        #         nowx = 0
+        #     elif nowy < 0:
+        #         nowy = area_len
+        #     elif nowy > area_len:
+        #         nowy = 0
 
         if where == "вниз" or where == 's':
-            if nowy <= area_len-2:
-                try:
-                    if not area[nowy+1][nowx] == 'o':
-                        nowy += 1
-                        area[nowy][nowx] = 'X'
-                        area[nowy-1][nowx] = '.'
+            if nowy <= area_len - 2:
+                pass
+            try:
+                if not area[nowy + 1][nowx] == 'o':
+                    nowy += 1
+                    area[nowy][nowx] = 'X'
+                    area[nowy - 1][nowx] = '_'
+                else:
+                    print('Тут стена :|')
 
-                except:
-                    print('Вы дошли до края платформы!')
+            except:
+                print('Вы дошли до края платформы!')
 
 
         elif where == "вверх" or where == 'w':
-            if nowy >= area_len-3:
-                try:
+            if nowy >= area_len - 3:
+                pass
+            try:
 
-                    if not area[nowy-1][nowx] == 'o':
-                        nowy -= 1
-                        area[nowy][nowx] = 'X'
-                        area[nowy+1][nowx] = '.'
-                    else:
-                        print('Тут стена :|')
+                if not area[nowy - 1][nowx] == 'o':
+                    nowy -= 1
+                    area[nowy][nowx] = 'X'
+                    area[nowy + 1][nowx] = '_'
+                else:
+                    print('Тут стена :|')
 
-                except:
-                    print('Вы дошли до края платформы!')
+            except:
+                print('Вы дошли до края платформы!')
 
 
         elif where == "право" or where == 'd':
-            if nowx <= area_len-2:
-                try:
+            if nowx <= area_len - 2:
+                pass
+            try:
 
-                    if not area[nowy][nowx+1] == 'o':
-                        nowx += 1
-                        area[nowy][nowx] = 'X'
-                        area[nowy][nowx-1] = '.'
+                if not area[nowy][nowx + 1] == 'o':
+                    nowx += 1
+                    area[nowy][nowx] = 'X'
+                    area[nowy][nowx - 1] = '_'
+                else:
+                    print('Тут стена :|')
 
-                except:
-                    print('Вы дошли до края платформы!')
+            except:
+                print('Вы дошли до края платформы!')
 
 
         elif where == "лево" or where == "a":
-            if nowx >= area_len-3:
-                try:
-                    if not area[nowy][nowx-1] == 'o':
-                        nowx -= 1
-                        area[nowy][nowx] = 'X'
-                        area[nowy][nowx+1] = '.'
+            if nowx >= area_len - 3:
+                pass
+            try:
+                if not area[nowy][nowx - 1] == 'o':
+                    nowx -= 1
+                    area[nowy][nowx] = 'X'
+                    area[nowy][nowx + 1] = '_'
+                else:
+                    print('Тут стена :|')
 
-                except:
-                    print('Вы дошли до края платформы!')
+            except:
+                print('Вы дошли до края платформы!')
 
+        print(f'X={nowx}, Y={nowy}')
         print_map(area)
 
 
-game(get_area(4))
+game(get_area(int(input())))
